c5488b62591887dd6cf684f4da8273d8611d5761
Andrew Zhou <andrewzhou@berkeley.edu>
2025-10-24 12:40:19 -0700

    1431. Kids With the Greatest Number of Candies (Easy)

---
+++ 1431-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        n = max(candies)
+        return map(lambda x:x+extraCandies >= n, candies)
+        
