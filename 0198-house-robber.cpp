f051c069356991dd6b035ec5bd33b702ecfe45c2
Andrew Zhou <andrewzhou@berkeley.edu>
2026-07-30 14:32:26 -0700

    198. House Robber (Medium)

---
+++ 0198-house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev2 = 0, prev1 = 0;
+        for (int x : nums) {
+            int cur = max(prev2 + x, prev1);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return prev1;
+    }
+};
